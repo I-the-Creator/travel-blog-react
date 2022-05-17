e539fa030aecdb0a61a89ecbeb451de6d0c9977c
I-the-Creator <andrey.burluckiy@gmail.com>
2022-05-17 16:22:11 +0200

    Maps component -logis added

--- frontend/components/Map.js
+++ frontend/components/Map.js
@@ -1,8 +1,30 @@
-const Map = () => {
+import { useRef, useEffect } from 'react'
+import mapboxgl from "mapbox-gl"
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+const Map = ({ longitude, latitude }) => {
+
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAP_API
+
+    const mapContainer = useRef(null)
+    const map = useRef(null)
+
+    // get coordinates for map
+    useEffect(() => {
+        if (map.current) return
+
+        if(longitude, latitude) {
+            map.current = new mapboxgl.Map({
+                container: mapContainer.current,
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [longitude, latitude],
+                zoom: 5,
+            })
+        }
+
+    }, [longitude, latitude])
     return (
-        <nav>
-            Map
-        </nav>
+        <div ref={mapContainer} className="map"/>
     )
 }
 
